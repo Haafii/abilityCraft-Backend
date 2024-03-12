97da2b3990f6268067e51a40a03b83f41804f0d5
Haafii <hafismuhammedtk@gmail.com>
2024-03-12 15:58:31 +0530

    post for login

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 router.post('/register', registerUser);
-router.get('/login', loginUser);
+router.post('/login', loginUser);
 router.get('/current', currentUser);
 
 module.exports = router;
