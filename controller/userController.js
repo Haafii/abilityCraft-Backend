12c578b80294b1bd7a640ce523c37d84027ab9ca
Haafii <hafismuhammedtk@gmail.com>
2024-03-12 15:57:53 +0530

    register user complete

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,31 @@ const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(201).send('User registered');
+    const {fullName, username, password} = req.body
+    if(!fullName || !username || !password){
+        res.status(404);
+        throw new Error('Please fill all the fields');
+    }
+    const userAvailable = await User.findOne({username}) 
+    if (userAvailable) {
+        res.status(400);
+        throw new Error('User already registered');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(`Hashed Password ${hashedPassword}`);
+    const user = await User.create({
+        fullName,
+        username,
+        password: hashedPassword
+    })
+    res.status(201).send(user);
+    console.log(`User created ${user}`);
+    if(user){
+        res.status(201).json({_id: user.id, username: user.username,})
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 });
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -13,6 +13,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    date: {
+        type: Date,
+        default: Date.now
+    },
 })
 
 module.exports = mongoose.model('user', userSchema);
