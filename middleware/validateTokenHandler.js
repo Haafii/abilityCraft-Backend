c8774751675cdce2cc3dd31017434aa74158b736
Haafii <hafismuhammedtk@gmail.com>
2024-03-13 15:57:18 +0530

    middleware for token validation

---
+++ middleware/validateTokenHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require(jsonwebtoken);
+
+const validateToken = asyncHandler(async (req, res, next) => {
+    let token;
+    let authHeader = req.header.Authorization || req.header.authorization;
+    if(authHeader && authHeader.startsWith('Bearer')){
+        token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+            if(err){
+                res.status(401);
+                throw new Error("User is not Authorized");
+            }
+            console.log(decoded);
+        })
+    }
+});
+
+
+module.exports = validateToken;
