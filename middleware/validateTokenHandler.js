53a4f58d9890aa8e7b24beae55b44c9fe35345e9
Haafii <hafismuhammedtk@gmail.com>
2024-03-13 16:00:03 +0530

    validate token

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const jwt = require(jsonwebtoken);
+const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
     let token;
--- router/userRoutes.js
+++ router/userRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { registerUser } = require('../controller/usercONTROLLER.JS');
 const { loginUser } = require('../controller/usercONTROLLER.JS');
 const { currentUser } = require('../controller/usercONTROLLER.JS');
+const validateToken = require('../middleware/validateTokenHandler');
 const router = express.Router();
 
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/current', currentUser);
+router.get('/current', validateToken, currentUser);
 
 module.exports = router;
