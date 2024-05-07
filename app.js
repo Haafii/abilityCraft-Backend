cd9994e3ff13e39b6f4f142cdaf3936c1001c5bc
Haafii <hafismuhammedtk@gmail.com>
2024-05-07 22:25:28 +0530

    Add CORS middleware to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const gameRoutes = require('./router/gameRoutes');
 const connectDb = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const userRoutes = require('./router/userRoutes');
+const cors = require('cors');
 
 
 connectDb();
@@ -11,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 8501;
 
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/games',gameRoutes)
 app.use('/user', userRoutes);
